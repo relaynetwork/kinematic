fe0ffa9686773d56ab2d8ff851d9116b9e69597d
Relay Network <kburton@relaynetwork.com>
2012-07-30 17:47:16 -0400

    lein-release plugin: bumped version from 1.0.1 to 1.0.2-SNAPSHOT for next developemnt cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.relaynetwork/kinematic "1.0.1"
+(defproject com.relaynetwork/kinematic "1.0.2-SNAPSHOT"
   :description "Dynamic Web Development with Clojure"
   :dev-dependencies [[swank-clojure                        "1.4.2"]]
   :local-repo-classpath true
