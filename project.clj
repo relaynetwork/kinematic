33dc6e3499366dbf2de3a2aee4fbbac5618808d5
Relay Network <kburton@relaynetwork.com>
2012-07-30 18:11:57 -0400

    bumping etl-utils

--- project.clj
+++ project.clj
@@ -10,4 +10,4 @@
                      [org.clojure/tools.namespace          "0.1.0"]
                      [ring/ring-core                       "1.1.0"]
                      [ring/ring-jetty-adapter              "1.1.0"]
-                     [org.clojars.kyleburton/clj-etl-utils "1.3.0"]])
+                     [org.clojars.kyleburton/clj-etl-utils "1.3.4"]])
