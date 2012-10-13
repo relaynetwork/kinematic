24c92dbc08fda3f36f0deb9873210763ca2bcfc7
Kyle Burton <kyle.burton@gmail.com>
2012-10-13 16:44:06 -0400

    vai -> via

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description          "Dynamic Web Development with Clojure"
   :dev-dependencies     [[swank-clojure                        "1.4.2"]]
   :local-repo-classpath true
-  :lein-release         {:deploy-vai :clojars}
+  :lein-release     {:deploy-via :clojars :scm :git}
   :dependencies     [[org.clojure/clojure                  "1.2.1"]
                      [org.clojure/clojure-contrib          "1.2.0"]
                      [org.clojure/tools.namespace          "0.1.0"]
