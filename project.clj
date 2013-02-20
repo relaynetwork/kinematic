978932caddf76aa793e7f89cbb8bdad51309b3cc
NickM and Kyle <kburton@relaynetwork.com>
2013-02-20 17:10:00 -0500

    lein-release plugin: bumped version from 1.3.0 to 1.3.1-SNAPSHOT for next developemnt cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.relaynetwork/kinematic "1.3.0"
+(defproject com.relaynetwork/kinematic "1.3.1-SNAPSHOT"
   :description          "Dynamic Web Development with Clojure"
   :dev-dependencies     [[swank-clojure                        "1.4.2"]]
   :local-repo-classpath true
