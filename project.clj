e248e6daf6ca1f0a2a5f548ae84cda7e8f3eb73b
Paul and Kyle <kburton@relaynetwork.com>
2013-10-07 18:38:58 -0400

    lein-release plugin: bumped version from 1.3.4 to 1.3.5-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.relaynetwork/kinematic "1.3.4"
+(defproject com.relaynetwork/kinematic "1.3.5-SNAPSHOT"
   :description          "Dynamic Web Development with Clojure"
   :url                  "http://github.com/relaynetwork/kinematic"
   :lein-release         {:deploy-via :clojars :scm :git}
